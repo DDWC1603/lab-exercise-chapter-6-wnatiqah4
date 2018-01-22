27e8fdbafdd04c60ce325239cf689cf4fa855df8
wnatiqah4 <33883982+wnatiqah4@users.noreply.github.com>
2018-01-22 21:30:08 +0800

    task completed

--- String-Exercise-3.cpp
+++ String-Exercise-3.cpp
@@ -22,3 +22,29 @@ int main()
 
 	return 0;
 }
+
+//AFTER 
+//student name : WAN NURUL ATIQAH
+// matric no : A17DW2386
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char strPswd[80];
+	
+	cout<<"Enter password: "<<endl;
+	cin>>strPswd;
+	
+	if (strPswd == "atiqah")
+		
+		cout<<"Logged in "<<endl;
+	
+	else 
+	
+		cout<<"invalid password "<<endl;
+
+	return 0;
+}
