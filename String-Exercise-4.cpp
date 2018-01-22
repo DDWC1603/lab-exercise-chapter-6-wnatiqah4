13a73cb2518883d7cc5311cc6fa319326413cc23
wnatiqah4 <33883982+wnatiqah4@users.noreply.github.com>
2018-01-22 21:30:15 +0800

    task completed

--- String-Exercise-4.cpp
+++ String-Exercise-4.cpp
@@ -17,4 +17,30 @@ int main()
 
 	cout<<lowerToUpper<<endl;
 }
+
+//AFTER
+//student name : WAN NURUL ATIQAH
+//matric no : A17DW2386
+
+#include <iostream>
+#include <cstring>
+#include <locale>
+using namespace std;
+
+int main()
+{
+	char lowerToUpper[80];
+	int i=0;
+
+	strcpy(lowerToUpper, "This is a check");
+
+	for(i=0;i<80;i++)
+	{
+		lowerToUpper[i] = toupper(lowerToUpper[i]);
+
+	}
+
+	cout<<lowerToUpper<<endl;
 	
+	return 0;
+}
