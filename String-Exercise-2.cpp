e1a59c553acce575aab47fe4dee778e7d504f5dc
wnatiqah4 <33883982+wnatiqah4@users.noreply.github.com>
2018-01-22 21:30:00 +0800

    task completed

--- String-Exercise-2.cpp
+++ String-Exercise-2.cpp
@@ -15,3 +15,25 @@ int main(){
 	return 0;
 }
 
+//AFTER 
+// student name : WAN NURUL ATIQAH
+// matric no : A17DW2386
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+
+	char char1[11], char2[21];
+
+	strcpy(char1," haillo");
+	strcpy(char2," there");
+
+	cout<<char1<<endl;
+	cout<<char2<<endl;
+
+	return 0;
+}
+
